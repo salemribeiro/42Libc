5f5cbfd3a928e319a1c32cdbe4cd5faaf4228bbf
Studio Perseidas <aquarium@studioperseidas.com>
2020-05-07 20:55:31 -0300

    correção de erro de teste

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -5,31 +5,32 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: sfreitas <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/12/12 19:52:59 by sfreitas          #+#    #+#             */
-/*   Updated: 2020/02/27 15:30:58 by sfreitas         ###   ########.fr       */
+/*   Created: 2020/05/07 20:53:20 by sfreitas          #+#    #+#             */
+/*   Updated: 2020/05/07 20:53:30 by sfreitas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
+size_t	ft_strlcat(char *dest, const char *src, size_t size)
 {
-	unsigned int j;
-	unsigned int i;
+	size_t	tdest;
+	size_t	tsrc;
+	size_t	i;
+	size_t	c;
 
+	tdest = ft_strlen(dest);
+	tsrc = ft_strlen(src);
+	if (tdest >= size)
+		return (tsrc + size);
 	i = 0;
-	j = 0;
-	while (dest[j] != '\0')
-		j++;
-	while (1 + j + i < size && src[i] != '\0')
+	c = tdest;
+	while (src[i] != '\0' && c < (size - 1) && size != '\0')
 	{
-		dest[j + i] = src[i];
+		dest[c] = (char)src[i];
+		c++;
 		i++;
 	}
-	dest[j + i] = '\0';
-	while (src[i] != '\0')
-		i++;
-	if (size < j)
-		return (size + i);
-	return (j + i);
+	dest[c] = '\0';
+	return (tsrc + tdest);
 }
