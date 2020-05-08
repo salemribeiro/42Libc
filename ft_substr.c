959fce08609a6bd98f439b1c251f6690c992e875
Studio Perseidas <aquarium@studioperseidas.com>
2020-05-08 13:58:43 -0300

    Corrreção com ajuda do will

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: sfreitas <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/20 16:00:03 by sfreitas          #+#    #+#             */
-/*   Updated: 2020/05/08 13:44:47 by sfreitas         ###   ########.fr       */
+/*   Updated: 2020/05/08 13:56:09 by sfreitas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,24 +17,23 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	size_t	i;
 	size_t	j;
 	char	*valuesb;
-
+	int		tlen;
 	i = 0;
 	j = 0;
+
 	if (!s)
 		return (NULL);
-	if (ft_strlen(s) < start)
+	tlen = ft_strlen(s);
+	if (tlen < start)
 		return (ft_strdup(""));
+	if (tlen < (start + len))
+		len = (tlen - start);
 	valuesb = (char*)malloc((sizeof(char) * len) + 1);
 	if (!valuesb)
 		return (NULL);
-	while (s[i] != '\0')
-		i++;
-	if (i >= (start + len))
-	{
-		i = start;
-		while (s[i] != '\0' && i < (start + len))
-			valuesb[j++] = s[i++];
-	}
+	i = start;
+	while (s[i])
+		valuesb[j++] = s[i++];
 	valuesb[j] = '\0';
 	return (valuesb);
 }
