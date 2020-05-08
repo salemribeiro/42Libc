bb31588dccf0188e868951d310cb9a49386aa2f5
Salem Freitas ribeiro junio <sfreitas@c1r2p1.42sp.org.br>
2020-05-08 13:05:37 -0300

    Correcao substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: sfreitas <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/20 16:00:03 by sfreitas          #+#    #+#             */
-/*   Updated: 2020/05/08 11:56:02 by sfreitas         ###   ########.fr       */
+/*   Updated: 2020/05/08 12:57:11 by sfreitas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,4 +36,3 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	valuesb[j] = '\0';
 	return (valuesb);
 }
-strsub = ft_substr(str, 400, 20)
